1090f45f2d617b76375eae4893ed526397c679b3
Cosmin Cojocar <cmcosmin@gmail.com>
2019-09-22 22:30:34 +0300

    chore+feat: create a node server with express, body-parser & cors packages

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+
 // Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
+let projectData = {};
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
 /* Middleware*/
 
-//Here we are configuring express to use body-parser as middle-ware.
+// Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static("website"));
 
 // Spin up the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Weather Journal app listening on port: ${port}`);
+});
+
 // Callback to debug
 
 // Initialize all route with a callback function
@@ -19,3 +34,4 @@
 // Callback function to complete GET '/all'
 
 // Post Route
+
