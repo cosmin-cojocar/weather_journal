e92395b9f54ef9d93a296e571f317f5f1cdb47f4
Cosmin Cojocar <cmcosmin@gmail.com>
2019-09-22 21:46:02 +0300

    feat: add main layout and styles for our weather journal app

---
+++ website/app.js
@@ -0,0 +1,5 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let date = new Date();
+let todayDate = date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear();
